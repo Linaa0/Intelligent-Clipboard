193e55acad815f9f2ce91c52c5fd67b917a0c697
Linaa <ishimwelina06@gmail.com>
2026-09-18 21:38:44 +0200

    Created SnippetManager

--- src/main/java/com/clipboard/manager/manager/SnippetManager.java
+++ src/main/java/com/clipboard/manager/manager/SnippetManager.java
@@ -1,5 +1,36 @@
 package com.clipboard.manager.manager;
 
+import com.clipboard.manager.model.Snippet;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class SnippetManager {
-    
+
+    private final Map<Integer, Snippet> snippets = new LinkedHashMap<>();
+
+    private int nextId = 1;
+
+    public Snippet addSnippet(String content, String type) {
+
+        Snippet snippet = new Snippet(
+                nextId,
+                content,
+                type
+        );
+
+        snippets.put(nextId, snippet);
+
+        nextId++;
+
+        return snippet;
+    }
+
+    public Map<Integer, Snippet> getSnippets() {
+        return snippets;
+    }
+
+    public int getSize() {
+        return snippets.size();
+    }
 }
