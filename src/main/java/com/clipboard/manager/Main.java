fa028f392c86f15ee8272fd48725019fbdab813d
Linaa <ishimwelina06@gmail.com>
2026-09-21 22:40:00 +0200

    Created the monitor-runner

--- src/main/java/com/clipboard/manager/Main.java
+++ src/main/java/com/clipboard/manager/Main.java
@@ -1,7 +1,36 @@
 package com.clipboard.manager;
 
+import com.clipboard.manager.clipboard.ClipboardMonitor;
+import com.clipboard.manager.manager.SnippetManager;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(
+                "Intelligent Clipboard Manager started."
+        );
+
+        SnippetManager snippetManager =
+                new SnippetManager();
+
+        ClipboardMonitor clipboardMonitor =
+                new ClipboardMonitor(snippetManager);
+
+        while (true) {
+
+            clipboardMonitor.checkClipboard();
+
+            try {
+
+                Thread.sleep(500);
+
+            } catch (InterruptedException e) {
+
+                Thread.currentThread().interrupt();
+
+                break;
+            }
+        }
     }
 }
